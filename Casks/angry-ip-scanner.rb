0a160145122393e7aae3cdc1184d70e2114a0868
Tim Bolish <timbolish@MacBookPro.lan>
2025-07-22 00:30:22 -0700

    Add Angry IP Scanner cask

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -2,7 +2,8 @@ cask "angry-ip-scanner" do
   version "3.9.1"
   sha256 "373a203298d08124a36414b2d832960d7c1cb121b65126863640d0445100cb65" # This will be auto-filled by brew
 
-  url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-macArm64-#{version}.zip"
+  url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-macArm64-#{version}.zip",
+    verified: "github.com/angryip/ipscan/"
   name "Angry IP Scanner"
   desc "Fast and friendly network scanner"
   homepage "https://angryip.org/"
